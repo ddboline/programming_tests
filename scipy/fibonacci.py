d374ec85990056aada0cacb08607df533df5b725
Daniel Boline <daniel.boline@stonybrook.edu>
2014-11-18 19:49:46 -0500

    ...

--- scipy/fibonacci.py
+++ scipy/fibonacci.py
@@ -10,5 +10,20 @@ def fib( n ) :
         return 1
     return fib(n-1) + fib(n-2)
 
-for n in range( 0 , int(os.sys.argv[1]) ) :
-    print n , fib(n)
+#for n in range( 0 , int(os.sys.argv[1]) ) :
+    #print n , fib(n)
+
+fib_v_n0 = 0
+fib_v_n1 = 0
+for n in range( 0 , int( os.sys.argv[1] ) ) :
+    if n == 0 :
+        fib_v0 = 0
+        fib_v1 = 0
+    elif n == 1 :
+        fib_v0 = 0
+        fib_v1 = 1
+    else :
+        fib = fib_v1 + fib_v0
+        fib_v0 = fib_v1
+        fib_v1 = fib
+    print n , fib_v1 , fib_v0
