8acf3ee9241b9d30d4383193da408e92d40daa13
Daniel Boline <ddboline@gmail.com>
2014-11-19 12:07:16 -0500

    initial

---
+++ scipy/mbroadcast_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+
+def first_test() :
+    a = np.tile(np.arange(0, 40, 10), (3, 1)).T
+    print a
+    b = np.array([0, 1, 2])
+    print b
+    print b+a
+
+def second_test() :
+    import matplotlib
+    matplotlib.use( 'Agg' )
+    import matplotlib.pyplot as plt
+    x, y = np.arange(5), np.arange(5)[:, np.newaxis]
+    print x,y
+    distance = np.sqrt(x ** 2 + y ** 2)
+    print distance
+    plt.pcolor(distance)
+    plt.colorbar()
+    plt.savefig( 'mbroadcast.png' , format='png')
+
+def mbroadcast_test() :
+    mileposts = np.array([0, 198, 303, 736, 871, 1175, 1475, 1544,1913, 2448])
+    distance_array = np.abs(mileposts - mileposts[:, np.newaxis])
+    print distance_array
+    return
+
+# first_test()
+second_test()
+# mbroadcast_test()
