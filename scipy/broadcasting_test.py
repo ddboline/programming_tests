e34c5c6a7a72fbce95abeb4592b65fc7ffe03b49
Daniel Boline <ddboline@gmail.com>
2014-11-19 10:09:53 -0500

    initial

---
+++ scipy/broadcasting_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+def random_walk() :
+    n_stories = 1000 # number of walkers
+    t_max = 200
+    
+    t = np.arange( t_max )
+    steps = 2 * np.random.random_integers(0, 1, (n_stories, t_max)) - 1
+    # print np.unique(steps)
+    positions = np.cumsum(steps, axis=1)
+    sq_distance = positions**2
+    mean_sq_distance = np.mean(sq_distance, axis=0)
+    
+    plt.figure(figsize=(4, 3))
+    plt.plot(t, np.sqrt(mean_sq_distance), 'g.', t, np.sqrt(t), 'y-')
+    plt.xlabel(r"$t$")
+    plt.ylabel(r"$\sqrt{\langle (\delta x)^2 \rangle}$")
+    plt.show()
+    
+    return
+
+random_walk()
---
+++ scipy/populations_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+def pop_test() :
+    data = np.loadtxt( 'populations.txt' )
+    year, hares, lynxes, carrots = data.T
+    populations = data[:, 1:]
+    
+    # plt.axes([0.2, 0.1, 0.5, 0.8])
+    # plt.plot(year, hares, year, lynxes, year, carrots)
+    # plt.legend(('Hare', 'Lynx', 'Carrot'), loc=(1.05, 0.5))
+    # plt.show()
+    
+    print populations.mean(axis=0)
+    print populations.std(axis=0)
+    
+    return
+
+pop_test()
