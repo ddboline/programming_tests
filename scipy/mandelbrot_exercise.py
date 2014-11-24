3c03123d46a34aa2639c0bef021d58df9c2ba3e1
Daniel Boline <daniel.boline@stonybrook.edu>
2014-11-24 07:38:56 -0500

    initial

---
+++ scipy/mandelbrot_exercise.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import matplotlib
+matplotlib.use( 'Agg' )
+import matplotlib.pyplot as plt
+
+def compute_mandelbrot(N_max, some_threshold, nx, ny):
+    # A grid of c-values
+    x = np.linspace(-2, 1, nx)
+    y = np.linspace(-1.5, 1.5, ny)
+
+    c = x[:,np.newaxis] + 1j*y[np.newaxis,:]
+
+    # Mandelbrot iteration
+
+    z = c
+    for j in xrange(N_max):
+        z = z**2 + c
+
+    mandelbrot_set = (abs(z) < some_threshold)
+
+    return mandelbrot_set
+
+# Save
+
+mandelbrot_set = compute_mandelbrot(50, 50., 601, 401)
+
+plt.imshow(mandelbrot_set.T, extent=[-2, 1, -1.5, 1.5])
+plt.gray()
+plt.savefig('mandelbrot.png')
