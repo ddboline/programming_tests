8147d179326108db61f9ad09b5663b8ccd768d63
Daniel Boline <ddboline@gmail.com>
2014-11-24 10:34:54 -0500

    initial

---
+++ scipy/markov_chain_exercise.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import matplotlib
+matplotlib.use( 'Agg' )
+import matplotlib.pyplot as plt
+
+def prob( i , j ) :
+    return
+
+def first() :
+    trans_man =
+    
+    # ranmat = np.random.rand(
+    
+first()
