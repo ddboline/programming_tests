fca2d49ba090faa8b0b50813221f298706f02167
Daniel Boline <ddboline@gmail.com>
2014-12-09 13:24:46 +0000

    ...

--- cython/cos_func.pyx
+++ cython/cos_func.pyx
@@ -1,11 +1,7 @@
 #!/usr/bin/python
 
 from __future__ import division
-import numpy as np
-cimport numpy as np
-
-cdef extern from 'math.h' :
-    double cos(double arg)
+from libc.math cimport cos
 
 def cos_func(arg) :
     return cos(arg)
