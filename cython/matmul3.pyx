d69e9de788a8cdf8175d384fc308b193b6675714
Daniel Boline <ddboline@gmail.com>
2014-12-10 14:18:29 -0500

    modest speedup

--- cython/matmul3.pyx
+++ cython/matmul3.pyx
@@ -8,12 +8,19 @@ cimport cython
 cdef extern from "gsl_cblas.h":
     double ddot "cblas_ddot"(const int N, const double *X, const int incX, const double *Y, const int incY)
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
 def matmul3(np.ndarray[np.float64_t, ndim=2] A, np.ndarray[np.float64_t, ndim=2] B, np.ndarray[np.float64_t, ndim=2] out):
-    cdef Py_ssize_t i, j
-    cdef np.ndarray[np.float64_t, ndim=1] A_row, B_col
+    cdef Py_ssize_t i, j, k
+    #cdef np.ndarray[np.float64_t, ndim=1] A_row, B_col
+    cdef np.ndarray[np.float64_t, ndim=1] A_row = np.zeros(A.shape[1]), B_col = np.zeros(B.shape[0])
     for i in xrange(A.shape[0]):
-        A_row = A[i,:]
+        for k in xrange(A.shape[1]):
+            A_row[k] = A[i,k]
+        #A_row = A[i,:]
         for j in range(B.shape[1]):
-            B_col = B[:,j]
+            for k in xrange(B.shape[0]):
+                B_col[k] = B[k,j]
+            #B_col = B[:,j]
             out[i,j] = ddot( A_row.shape[0], <np.float64_t*> A_row.data, A_row.strides[0] // sizeof(np.float64_t),
                                              <np.float64_t*> B_col.data, B_col.strides[0] // sizeof(np.float64_t) )
