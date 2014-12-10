d1dc52bdcd3c927623c50a2c2908fda7371c2192
Daniel Boline <ddboline@gmail.com>
2014-12-10 13:43:50 -0500

    ...

--- cython/setup.py
+++ cython/setup.py
@@ -5,10 +5,13 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 extensions = [
+    Extension("matmul2", ["matmul2.pyx"]),
     Extension("matmul3", ["matmul3.pyx"],
         include_dirs = ['/usr/include/gsl/'],
-        libraries = ['gsl'],
+        libraries = ['gslcblas'],
         library_dirs = ['/usr/lib']),
+    Extension('cos_func', ['cos_func.pyx'],
+                 libraries = ['m'] )
 ]
 setup(
     ext_modules = cythonize(extensions),
