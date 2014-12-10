247db84811653d355c69346048dfa8ae6fc92261
Daniel Boline <ddboline@gmail.com>
2014-12-10 14:18:03 -0500

    ...

--- cython/integ.c
+++ cython/integ.c
@@ -3,11 +3,12 @@
 #include <sys/types.h>
 #include <stdbool.h>
 #include <complex.h>
+#include <stdint.h>
 
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long int uint64_t;
+// typedef unsigned char uint8_t;
+// typedef unsigned short uint16_t;
+// typedef unsigned int uint32_t;
+// typedef unsigned long int uint64_t;
 
 long double f( long double x ) {
     return x*x - x;
