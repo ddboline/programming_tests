75ea50a90d79b8d239688ddbfb3c53b2fae9e34c
Daniel Boline <ddboline@gmail.com>
2014-12-10 10:58:34 -0500

    simplify a bit

--- cython/run_primes.py
+++ cython/run_primes.py
@@ -5,12 +5,19 @@ pyximport.install()
 
 import primes
 import primes2
-import time
+import time , os
 
-t0 = time.clock()
+os.system('gcc primes.c -o primes')
+
+ts = []
+
+ts.append( time.clock() )
 print primes.primes(10000)
-t1 = time.clock()
+ts.append( time.clock() )
 print primes2.primes(10000)
-t2 = time.clock()
+ts.append( time.clock() )
+os.system( './primes 10000' )
+ts.append( time.clock() )
 
-print t2-t1, t1-t0
+for i in range(0,len(ts)-1):
+    print ts[i+1] - ts[i]
