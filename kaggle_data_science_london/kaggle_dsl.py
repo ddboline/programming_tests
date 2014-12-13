ec6fce79ed893852ad9f9af249678245cc2b70a0
Daniel Boline <daniel.boline@stonybrook.edu>
2014-12-13 09:46:59 -0500

    various improvements

--- kaggle_data_science_london/kaggle_dsl.py
+++ kaggle_data_science_london/kaggle_dsl.py
@@ -7,7 +7,7 @@ import pylab as pl
 import numpy as np
 from sklearn import linear_model
 from sklearn import svm, neighbors , svm , grid_search
-import csv
+import csv , os
 import pandas as pd
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -22,14 +22,13 @@ from sklearn.qda import QDA
 from sklearn.mixture import GMM
 from sklearn.decomposition import PCA
 
-def kaggle_submit( model , xtrain , ytrain , pca ):
+def kaggle_submit( model , xtrain , ytrain , pca=None ):
     print model.fit(xtrain,ytrain)
 
     xtest = pd.read_csv('test.csv', header=None).as_matrix()
-    ytest = model.predict( xtest )
-    
     if pca:
         xtest = pca.transform(xtest)
+    ytest = model.predict( xtest )
 
     with open('submit.csv', 'w') as outfile:
         outfile.write( 'Id,Solution\n' )
@@ -38,38 +37,49 @@ def kaggle_submit( model , xtrain , ytrain , pca ):
         for n , out in enumerate(ytest):
             ow.writerow( [ n+1 , out ] )
 
-def kaggle_dsl_scoring():
+def kaggle_dsl_scoring(gauss_train_size=1000):
     xt = pd.read_csv('train.csv', header=None).as_matrix()
     yt = pd.read_csv('trainLabels.csv', header=None).as_matrix()
     xtest = pd.read_csv('test.csv', header=None).as_matrix()
+        
+    pca = None
+    pca = PCA(n_components=12)
+    pca.fit(xtest)
+    xtp = pca.transform(xt)
+
+    i0 = np.argwhere(yt==0)[:, 0]
+    i1 = np.argwhere(yt==1)[:, 0]
+    x0_mean = np.mean(xtp[i0, :], axis=0)
+    x0_cov = np.cov(xtp[i0, :], rowvar=0)
+    x1_mean = np.mean(xtp[i1, :], axis=0)
+    x1_cov = np.cov(xtp[i1, :], rowvar=0)
     
-    print np.mean(xt[yt==0], axis=0)
-    print np.cov(xt[yt==0])
-    print np.mean(xt[yt==1])
-    print np.cov(xt[yt==1])
-    
-    #pca = PCA(n_components=12)
-    #pca.fit(xt)
-    #xtp = pca.transform(xt)
+    #gauss_train_size = 7000
+    xtg0 = np.random.multivariate_normal( x0_mean , x0_cov , gauss_train_size )
+    xtg1 = np.random.multivariate_normal( x1_mean , x1_cov , gauss_train_size )
+
+    xt = np.r_[xtg0, xtg1]
+    yt = np.r_[[0]*gauss_train_size, [1]*gauss_train_size]
     
     gammas = np.logspace(-6, -1, 10)
     svc = svm.SVC()
     clf = grid_search.GridSearchCV(estimator=svc, param_grid=dict(gamma=gammas), n_jobs=-1)
 
-    classifier_dict = { 'gridCV': clf,
-                'linear_model': linear_model.LogisticRegression(fit_intercept=False,penalty='l1'),
-                'linSVC': svm.LinearSVC(),
-                'kNC5': KNeighborsClassifier(),
-                'kNC6': KNeighborsClassifier(6),
-                'SVC': SVC(kernel="linear", C=0.025),
-                'SVC2': SVC(gamma=2, C=1),
-                'DT': DecisionTreeClassifier(max_depth=5),
-                'RF': RandomForestClassifier(),
+    classifier_dict = { 
+                'gridCV': clf,
+                #'linear_model': linear_model.LogisticRegression(fit_intercept=False,penalty='l1'),
+                #'linSVC': svm.LinearSVC(),
+                #'kNC5': KNeighborsClassifier(),
+                #'kNC6': KNeighborsClassifier(6),
+                #'SVC': SVC(kernel="linear", C=0.025),
+                #'DT': DecisionTreeClassifier(max_depth=5),
+                'RF': RandomForestClassifier(n_estimators=200),
                 'Ada': AdaBoostClassifier(),
-                'Gauss': GaussianNB(),
-                'LDA': LDA(),
+                #'Gauss': GaussianNB(),
+                #'LDA': LDA(),
                 'QDA': QDA(),
-                #'GMM': GMM()
+                #'GMM': GMM(),
+                'SVC2': SVC(),
                 }
 
     classifier_list = classifier_dict.values()
@@ -79,12 +89,11 @@ def kaggle_dsl_scoring():
     
     for idx in range(1):
         randperm = np.random.permutation( np.arange( xt.shape[0] ) )
-        xtrain = xt[randperm[:500],:]
-        ytrain = yt[randperm[:500],0]
-        xtest = xt[randperm[500:],:]
-        ytest = yt[randperm[500:],0]
-    
-        #pca = PCA(whiten=True)
+        xtrain = xt[randperm[::2],:]
+        ytrain = yt[randperm[::2]]
+        xtest = xt[randperm[1::2],:]
+        ytest = yt[randperm[1::2]]
+
         #pca.fit(xt)
         #xtrain = pca.transform(xtrain)
         #xtest = pca.transform(xtest)
@@ -95,10 +104,13 @@ def kaggle_dsl_scoring():
         #xtest = gmm.transform(xtest)
     
         def score_model( model ):
-            model.fit(xtrain, ytrain)
-            # if hasattr( model, 'coef_' ):
-                # print model.coef_[0]
-            return model.score(xtest, ytest) , model
+            try:
+                model.fit(xtrain, ytrain)
+                #print xtest.shape, ytest.shape
+                return model.score(xtest, ytest) , model
+            except:
+                print model
+                exit(0)
     
         for k , c in classifier_dict.iteritems():
             s , m = score_model( c )
@@ -108,8 +120,9 @@ def kaggle_dsl_scoring():
     for s , k in ksarr:
         print np.mean(s) , k
     
+    print xt.shape, xtest.shape, yt.shape, ytest.shape
     return classifier_dict[ksarr[-1][1]] , xt , yt , pca
     
 if __name__ == '__main__':
-    model , x , y , pca = kaggle_dsl_scoring()
+    model , x , y , pca = kaggle_dsl_scoring(gauss_train_size=int(os.sys.argv[1]))
     kaggle_submit( model , x , y , pca )
