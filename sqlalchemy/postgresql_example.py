803d124132cd41f48d66d84e29c0cbd1596559d1
Daniel Boline <ddboline@gmail.com>
2014-12-18 13:32:29 -0500

    ...

--- sqlalchemy/postgresql_example.py
+++ sqlalchemy/postgresql_example.py
@@ -6,8 +6,11 @@ import pandas as pd
 
 def postgresql_example():
     engine = create_engine( 'postgresql://ddboline:BQGIvkKFZPejrKvX@localhost/mydb' )
+    con = engine.connect()
     
-    dat = pd.read_sql( 'person' )
+    dat = pd.read_sql( 'select * from person' , con )
+    
+    print dat
 
 if __name__ == '__main__':
     
