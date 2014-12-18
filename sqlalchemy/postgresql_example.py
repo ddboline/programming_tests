81e7f44e4b908b73f1f0e95b3de2a9391e35cdc3
Daniel Boline <daniel.boline@stonybrook.edu>
2014-12-18 13:48:43 -0500

    ...

--- sqlalchemy/postgresql_example.py
+++ sqlalchemy/postgresql_example.py
@@ -5,10 +5,10 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 def postgresql_example():
-    engine = create_engine( 'postgresql://ddboline@localhost/mydb' )
+    engine = create_engine( 'postgresql://ddboline:BQGIvkKFZPejrKvX@localhost/mydb' )
     con = engine.connect()
     
-    dat = pd.read_sql( 'select * from person' , con )
+    dat = pd.read_sql( 'select * from person;' , con )
     
     print dat
 
