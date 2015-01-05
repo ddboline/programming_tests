690675a3ebde452db394e85230a8cb2b10a1aff7
Daniel Boline <daniel.boline@stonybrook.edu>
2015-01-05 14:39:40 -0500

    ...

--- sqlalchemy/setup_cloud9.sh
+++ sqlalchemy/setup_cloud9.sh
@@ -3,4 +3,15 @@
 # git clone https://github.com/ddboline/programming_tests.git
 
 sudo apt-get install -y python-pip
-sudo pip install pandas sqlalchemy
+
+### building pandas sucks up memory, create a swap file if there are problems
+# sudo dd if=/dev/zero of=/swapfile bs=1024 count=256k
+# sudo chown root:root /swapfile 
+# sudo chmod 0600 /swapfile
+# sudo mkswap /swapfile
+# sudo swapon /swapfile
+
+sudo pip install --upgrade sqlalchemy
+sudo pip install --upgrade numpy
+sudo pip install --upgrade pandas 
+sudo pip install --upgrade blaze
