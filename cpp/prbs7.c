16ead4962e002b110db19b84a536a36e0a8c715e
Daniel Boline <ddboline@gmail.com>
2015-01-14 09:57:14 -0500

    prbs7

---
+++ cpp/prbs7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+ 
+int main(int argc, char** argv) {
+    uint8_t start = 0x02;
+    if(argc>1){
+        start = atoi(argv[1]);
+        printf("%s %x\n", argv[1], start);
+    }
+    uint8_t a = start;
+    int i = 1;
+ 
+    for(i = 1;; i++) {
+        int newbit = (((a >> 6) ^ (a >> 5)) & 1);
+        a = ((a << 1) | newbit) & 0x7f;
+        printf("%x", a);
+        if (a == start) {
+            printf("\nrepetition period is %d\n", i);
+            break;
+        }
+    }
+}
