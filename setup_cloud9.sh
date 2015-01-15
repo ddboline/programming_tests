b94ad37c81d6a8a9878a952bd573bc5c0ca64a29
Daniel Boline <ddboline@gmail.com>
2015-01-15 17:17:15 -0500

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-./scipy/setup_cloud9.sh
-./python/setup_cloud9.sh
-./numpy/setup_cloud9.sh
-./postgresql/setup_cloud9.sh
-./mysql/setup_cloud9.sh
-./cython/setup_cloud9.sh
-./boost_python/setup_cloud9.sh
-./sqlalchemy/setup_cloud9.sh
+for DIR in scipy python numpy postgresql mysql cython boost_python sqlalchemy;
+do
+    sh setup_cloud9.sh
+done
+
