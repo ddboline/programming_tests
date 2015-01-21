2c81a980bbc1fa203f97268d581a79ef3d2d1a63
Daniel Boline <ddboline@gmail.com>
2015-01-21 10:13:25 -0500

    failure

--- mysql/setup_cloud9.sh
+++ mysql/setup_cloud9.sh
@@ -4,15 +4,11 @@
 
 sudo apt-get install -y mysql-server
 
-# sudo -u postgres createuser -E -e $USER
-# sudo -u postgres psql -c "CREATE ROLE $USER PASSWORD 'BQGIvkKFZPejrKvX' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;" 
-# sudo -u postgres createdb mydb
-
 sudo mysql_install_db
 sudo /usr/bin/mysql_secure_installation
 
-for N in `seq 0 20`; 
-do 
-    psql mydb $USER < ../postgresql/test${N}.sql ; 
+for N in `seq 0 20`;
+do
+    mysql --user=$USER --password=BQGIvkKFZPejrKvX mydb < ../postgresql/test${N}.sql;
 done
 
