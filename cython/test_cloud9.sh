b280057b6eee7c4b7f5b64c2b85928a91a101583
Daniel Boline <ddboline@gmail.com>
2015-01-21 10:26:31 -0500

    new

---
+++ cython/test_cloud9.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Basel"
+./run_basel.py
+echo "Chudnovsky"
+./run_chudnovsky.py
+echo "Cosine"
+./run_cos.py
+echo "Fibonacci"
+./run_fibonacci.py
+echo "Integral"
+./run_integ.py
+echo "Integral Approx"
+./run_integral_approx.py
+echo "Matrix Multiplication"
+./run_matmul.py 100 100 100
+echo "Pi Wallis"
+./run_pi_wallis.py
