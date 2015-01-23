1bbfca47044c3f05be6495710f1a60d953191b06
Daniel Boline <daniel.boline@stonybrook.edu>
2015-01-23 15:51:47 -0500

    initial import

---
+++ python3/stock_parser.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3.4
+# -*- coding: utf-8 -*-
+
+import multiprocessing
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+
+def read_stock_url(symbol):
+    for line in urlopen("http://finance.yahoo.com/q?s=" + symbol.lower() + "&ql=0"):
+        if 'yfs_l84_%s' % symbol.lower() in line:
+            price = float(line.split('yfs_l84_%s\">' % symbol.lower())[1].split('</')[0].replace(',',''))
+            return symbol.upper(), price
+    return symbol.upper(), -1
+
+def run_stock_parser():
+    stock_symbols = []
+    with open('symbols.txt','r') as symfile:
+        for n, line in enumerate(symfile):
+            sym = line.strip()
+            if sym:
+                stock_symbols.append(sym)
+    pool = multiprocessing.Pool(5)
+    for symbol, price in pool.imap_unordered(read_stock_url, stock_symbols):
+        print symbol, price
+
+if __name__ == '__main__':
+    run_stock_parser()
---
+++ python3/unittests.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3.4
+# -*- coding: utf-8 -*-
+"""
+    Unit testing testing...
+"""
+
+import unittest
+import datetime
+import analyze_gmail
+
+class TestAnalyzeGmail(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_tv_job_constructor(self):
+        pass
+
+    def test_parse_quoted_email_string(self):
+        inp_str = '"Active.com Email Exclusive" <active_offers@news1-active.com>k.com>'
+        output = analyze_gmail.parse_quoted_email_string(inp_str)
+        self.assertEqual(output[0], 'active_offers@news1-active.com')
+
+if __name__ == '__main__':
+    unittest.main()
