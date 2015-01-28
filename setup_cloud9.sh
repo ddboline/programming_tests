63d8b8a54b3a9cf017cf9b502f7d0813128a240f
Daniel Boline <ddboline@gmail.com>
2015-01-28 14:44:39 -0500

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for DIR in scipy python numpy cython boost_python sqlalchemy scipy sklearn;
+for DIR in scipy python numpy cython boost_python sqlalchemy scipy sklearn world_records;
 do
     cd $DIR
     sh setup_cloud9.sh
