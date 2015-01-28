b199cd778dcadd4b379030b0e8d230703e49a081
Daniel Boline <ddboline@gmail.com>
2015-01-28 13:53:10 -0500

    ...

--- python3/stock_parser.py
+++ python3/stock_parser.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
 
 import multiprocessing
 from bs4 import BeautifulSoup
--- python3/unittests.py
+++ python3/unittests.py
@@ -1,12 +1,8 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
     Unit testing testing...
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
 
 import unittest
 import datetime
