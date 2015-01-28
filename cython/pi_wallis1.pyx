bdfa5cca2ef85f128648156fc102101a61d300f5
Daniel Boline <ddboline@gmail.com>
2015-01-28 09:33:01 -0500

    lower N, avoid int double conversion, both seem required for arm

--- cython/pi_wallis1.pyx
+++ cython/pi_wallis1.pyx
@@ -4,7 +4,8 @@ import os
 
 cpdef pi_wallis(int n = 10):
     cdef double val = 2.
-    cdef int i = 1
+    cdef double preval = 0.
+    cdef double i = 1
     for i in xrange(1,n):
         val *= (4.*i**2) / (4.*i**2 - 1.)
     return val
