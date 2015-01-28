064fff2558bc7ef1d778978f368b5989061aedb5
Daniel Boline <ddboline@gmail.com>
2015-01-28 16:03:45 -0500

    scipy was duplicated

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for DIR in scipy python numpy cython boost_python sqlalchemy scipy sklearn scipy world_records;
+for DIR in scipy python numpy cython boost_python sqlalchemy sklearn scipy world_records;
 do
     cd $DIR
     sh test_cloud9.sh
