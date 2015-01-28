d35fcee6653a5e29c6f9236360f74fb5f16443dc
Daniel Boline <ddboline@gmail.com>
2015-01-28 10:33:43 -0500

    some changes to run in headless env

--- numpy/basemap_test.py
+++ numpy/basemap_test.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from mpl_toolkits.basemap import Basemap
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as pl
+from mpl_toolkits.basemap import Basemap
+
 # llcrnrlat,llcrnrlon,urcrnrlat,urcrnrlon
 # are the lat/lon values of the lower left and upper right corners
 # of the map.
 # lat_ts is the latitude of true scale.
 # resolution = 'c' means use crude resolution coastlines.
+
 m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, \
             llcrnrlon=-180, urcrnrlon=180, lat_ts=20, resolution='c')
 m.drawcoastlines()
@@ -18,7 +22,7 @@ m.drawparallels(np.arange(-90., 91., 30.))
 m.drawmeridians(np.arange(-180., 181., 60.))
 m.drawmapboundary(fill_color='aqua')
 m.drawstates()
-plt.title("Mercator Projection")
-plt.show()
+pl.title("Mercator Projection")
+pl.show()
 
-print dir(m)
+pl.savefig('basemap.png')
