84f3e257023c36445dd270618df74aba4c889f89
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-08 22:35:22 -0500

    ...

--- python/unittests.py
+++ python/unittests.py
@@ -17,9 +17,6 @@ class TestAnalyzeGmail(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_tv_job_constructor(self):
-        pass
-
     def test_parse_quoted_email_string(self):
         inp_str = '"Active.com Email Exclusive" <active_offers@news1-active.com>k.com>'
         output = analyze_gmail.parse_quoted_email_string(inp_str)
