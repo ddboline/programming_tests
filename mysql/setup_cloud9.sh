a4e73e48eaca7cc0d1218e336d259e448d7aa7d9
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-28 08:58:59 -0500

    ...

--- mysql/setup_cloud9.sh
+++ mysql/setup_cloud9.sh
@@ -7,6 +7,11 @@ sudo apt-get install -y mysql-server
 sudo mysql_install_db
 sudo /usr/bin/mysql_secure_installation
 
+sudo mysql --user=root mysql --execute="CREATE USER 'ddboline'@'%' IDENTIFIED BY 'BQGIvkKFZPejrKvX';"
+sudo mysql --user=root mysql --execute="GRANT ALL PRIVILEGES ON *.* TO 'ddboline'@'%' WITH GRANT OPTION;"
+
+mysql --user=$USER --password=BQGIvkKFZPejrKvX --execute="CREATE DATABASE IF NOT EXISTS mydb"
+
 for N in `seq 0 20`;
 do
     mysql --user=$USER --password=BQGIvkKFZPejrKvX mydb < ../postgresql/test${N}.sql;
