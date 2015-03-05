d1cf02a732b20e4925bd4127e0af73bed6238e7b
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-04 21:35:39 -0500

    ...

--- sqlalchemy/read_hpodder.py
+++ sqlalchemy/read_hpodder.py
@@ -5,11 +5,23 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 def read_hpodder():
-    engine = create_engine('sqlite:///hpodder.db', echo=True)
+    engine = create_engine('sqlite:///hpodder.db', echo=False)
     con = engine.connect()
-    result = con.execute("select * from podcasts limit 10;")
+
+    for line in con.execute("select * from sqlite_master where type='table';"):
+        if 'CREATE TABLE' in line[4]:
+                print line[4].replace(',', ',\n')
+    
+    for line in con.execute("select name from sqlite_master where type='table';"):
+        print line
+    
+    for line in con.execute("select feedurl from podcasts;"):
+        print line
+
+    result = con.execute("select epurl, status from episodes limit 10;")
     for line in result:
         print line
+
     return
 
 if __name__ == '__main__':
