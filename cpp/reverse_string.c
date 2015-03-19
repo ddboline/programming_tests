1d312cdf19603a98f9e0e15a1d5beeba29d09fb4
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-19 06:04:06 -0400

    inplace string reversal

--- cpp/reverse_string.c
+++ cpp/reverse_string.c
@@ -12,6 +12,18 @@ char * reverse_string( const char * inp_str ){
     return newstr;
 }
 
+int reverse_string_inplace(char * inp_str){
+    int inp_str_len = strlen(inp_str);
+    int idx = 0;
+    char temp;
+    for(idx=0; idx < inp_str_len/2; idx++){
+        temp = inp_str[idx];
+        inp_str[idx] = inp_str[inp_str_len-idx-1];
+        inp_str[inp_str_len-idx-1] = temp;
+    }
+    return 0;
+}
+
 int main(int argc, char ** argv){
     /* Silly little program to reverse a string*/
     char * rev_string = NULL;
@@ -23,18 +35,14 @@ int main(int argc, char ** argv){
     for( idx = (argc-1) ; idx >= 1 ; idx-- ) {
         rev_string = reverse_string( argv[idx] );
         printf( "%s " , rev_string );
-        free(rev_string);
     }
     printf( "\n" );
-    
-    // char * teststr = "I'm feeling lucky!";
-    // int teststr_len = strlen( teststr );
-    // char * newstr = malloc( teststr_len+1 );
-    // memset( newstr , 0 , teststr_len+1 );
-    // int idx = 0;
-    // for( idx=0 ; idx < strlen(teststr) ; idx++ ) {
-    //     newstr[idx] = teststr[teststr_len-idx-1];
-    //     printf( "%d %d %c %c\n" , idx , teststr_len-idx-1 , newstr[idx] , teststr[teststr_len-idx-1] );
-    // }
-    // printf( "%s\n" , newstr );
+    for( idx = (argc-1) ; idx >= 1 ; idx-- ) {
+        strcpy(rev_string, argv[idx]);
+        reverse_string_inplace(rev_string);
+        printf( "%s " , rev_string );
+    }
+    printf( "\n" );
+    free(rev_string);
+    return 0;
 }
