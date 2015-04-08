9799d426caab9940fa13d6aeb3e007960738ae9b
Daniel Boline <ddboline@gmail.com>
2015-04-07 21:57:34 -0400

    ...

--- python/stock_parser.py
+++ python/stock_parser.py
@@ -9,27 +9,32 @@ import multiprocessing
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 
-def read_stock_url(symbol):
-    for line in urlopen("http://finance.yahoo.com/q?s=" + symbol.lower() + "&ql=0"):
-        line = unicode(line, errors='ignore')
-        if 'yfs_l84_%s' % symbol.lower() in line:
-            price = float(line.split('yfs_l84_%s\">' % symbol.lower())[1].split('</')[0].replace(',',''))
-            return symbol.upper(), price
-    return symbol.upper(), -1
+def read_stock_url(symbol_q, price_q):
+    while True:
+        while not symbol_q.empty():
+            symbol = symbol_q.get()
+            if symbol == 'EMPTY':
+                return True
+            for line in urlopen("http://finance.yahoo.com/q?s=" + symbol.lower() + "&ql=0"):
+                line = unicode(line, errors='ignore')
+                if 'yfs_l84_%s' % symbol.lower() in line:
+                    price = float(line.split('yfs_l84_%s\">' % symbol.lower())[1].split('</')[0].replace(',',''))
+                    price_q.put((symbol.upper(), price))
 
 def write_output_file(price_q):
     with open('stock_prices.csv', 'w') as outfile:
         outfile.write('Stock,Price\n')
         while True:
             while not price_q.empty():
-                try:
-                    s, p = price_q.get()
-                except TypeError:
-                    return
+                vals = price_q.get()
+                if vals == 'EMPTY':
+                    return True
+                s, p = vals
                 outfile.write('%s,%s\n' % (s, p))
             outfile.flush()
 
 def run_stock_parser():
+    symbol_q = multiprocessing.Queue()
     price_q = multiprocessing.Queue()
     
     stock_symbols = []
@@ -39,20 +44,24 @@ def run_stock_parser():
             if sym:
                 stock_symbols.append(sym)
 
-    ncpu = len(filter(lambda x: x.find('processor')==0, 
+    ncpu = len(filter(lambda x: x.find('processor')==0,
                       open('/proc/cpuinfo')
                       .read().split('\n')))
 
-    pool = multiprocessing.Pool(ncpu*2)
+    pool = [multiprocessing.Process(target=read_stock_url, args=(symbol_q, price_q,)) for _ in range(ncpu*2)]
+    for p in pool:
+        p.start()
     output = multiprocessing.Process(target=write_output_file, args=(price_q,))
     output.start()
-    
-    for symbol, price in pool.imap_unordered(read_stock_url, stock_symbols):
-        price_q.put([symbol, price])
-    price_q.put(None)
+
+    for symbol in stock_symbols:
+        symbol_q.put(symbol)
+    for p in pool:
+        symbol_q.put('EMPTY')
+    for p in pool:
+        p.join()
+    price_q.put('EMPTY')
     output.join()
-    pool.close()
-    pool.join()
 
 if __name__ == '__main__':
     run_stock_parser()
