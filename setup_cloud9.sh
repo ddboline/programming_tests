ec47f37a27f12432b78fb27f849c4d7af977cff0
Daniel Boline <daniel.boline@stonybrook.edu>
2015-04-29 21:14:30 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for DIR in scipy python numpy cython boost_python sqlalchemy sklearn;
+for DIR in scipy python numpy cython boost_python sqlalchemy sklearn scala;
 do
     cd $DIR
     sh setup_cloud9.sh
