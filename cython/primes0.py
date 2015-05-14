05727a1ab4b4ef3a6c274ab5b16f55e1d5850fd8
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-14 06:33:24 -0400

    change variable names for clarity

--- cython/primes0.py
+++ cython/primes0.py
@@ -3,19 +3,17 @@
 def primes(kmax):  # The argument will be converted to int or raise a TypeError.
     #cdef int n, k, i  # These variables are declared with C types.
     #cdef int p[1000] # Another C type
-    p = [0]*10000
-    result = 1
-    if kmax > 10000:
-        kmax = 10000
-    k = 0
-    n = 2
-    while k < kmax:
+    primes = []
+    largest_prime = 1
+    n_primes = 0
+    prime_candidate = 2
+    while n_primes < kmax:
         i = 0
-        while i < k and n % p[i] != 0:
+        while i < n_primes and prime_candidate % primes[i] != 0:
             i += 1
-        if i == k:
-            p[k] = n
-            k += 1
-            result = n
-        n += 1
-    return result
+        if i == n_primes:
+            primes.append(prime_candidate)
+            n_primes += 1
+            largest_prime = prime_candidate
+        prime_candidate += 1
+    return largest_prime
