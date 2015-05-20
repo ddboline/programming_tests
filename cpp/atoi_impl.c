91c3b379c288f4b54cbfede5e39b93a057d53d39
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-20 02:42:29 -0400

    cleanup

--- cpp/atoi_impl.c
+++ cpp/atoi_impl.c
@@ -4,23 +4,23 @@
 #include <stdint.h>
 
 int64_t atoi_impl( const char * inpstr ){
-    int64_t outp = 0 ;
-    int idx, ascii_code;
-    int64_t power = 1;
+    int64_t outp = 0 , power = 1;
+    int idx = 0, ascii_code;
     int inpstr_len = strlen( inpstr );
     char ascii_offset = '0';
-    // printf( "length %d\n" , inpstr_len );
+    char current_char = *inpstr;
     for( idx = inpstr_len-1 ; idx >= 0 ; idx-- ){
         ascii_code = inpstr[idx];
-        if( ascii_code < ascii_offset || ascii_code > ascii_offset+9 ) {
-            if( ascii_code == '+' ) continue;
-            else if( ascii_code == '-' ) outp *= -1;
-            else return outp;
-            continue;
+        if(ascii_code == '+')
+            return outp;
+        else if(ascii_code == '-'){
+            outp *= -1;
+            return outp;
         }
-        outp += ( ascii_code - ascii_offset ) * power;
-        power *= 10;
-        // printf( "%d 0x%x %lld\n" , idx , inpstr[idx] , outp );
+        else if(ascii_code >= ascii_offset && ascii_code <= ascii_offset+9){
+            outp += (ascii_code-ascii_offset)*power;
+            power *= 10;
+        }
     }
     return outp;
 }
@@ -29,6 +29,7 @@ int main( int argc , char ** argv ) {
     int idx = 0;
     for( idx = 1 ; idx < argc ; idx++ ) {
         printf( "%s\n" , argv[idx] );
+        printf( "%ld\n" , atol( argv[idx] ) );
         printf( "%ld\n" , atoi_impl( argv[idx] ) );
     }
     return 0;
