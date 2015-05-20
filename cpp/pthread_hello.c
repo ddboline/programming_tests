eb52726ae56a42e82c2651c59926791dd57c77c2
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-20 02:45:51 -0400

    ...

--- cpp/pthread_hello.c
+++ cpp/pthread_hello.c
@@ -9,14 +9,19 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #define NUM_THREADS	5
 
 void *PrintHello(void *threadid)
 {
-   long tid;
-   tid = (long)threadid;
-   printf("Hello World! It's me, thread #%ld!\n", tid);
-   pthread_exit(NULL);
+    int idx = 0;
+    for(idx=0; idx < 10; idx++){
+        long tid;
+        tid = (long)threadid;
+        printf("Hello World! It's me, thread #%ld, it %d!\n", tid, idx);
+        sleep(1);
+    }
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
