b36756a0b13c2d12b303a2750ae28f6f707c8a4b
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-22 01:16:03 -0400

    os.popen/os.system conversion

--- cython/run_smith_number.py
+++ cython/run_smith_number.py
@@ -5,9 +5,10 @@ pyximport.install()
 
 import smith_number0
 import smith_number1
-import time, os
+import time
+from subprocess import call, Popen, PIPE
 
-os.system('g++ -O3 smith_number.cpp -o smith_number')
+call('g++ -O3 smith_number.cpp -o smith_number', shell=True)
 
 ts = []
 
@@ -17,7 +18,9 @@ for N in (4, 22, 27, 28, 58, 59, 85, 94, 121, 4937775, 2050918644):
     ts0 = time.clock()
     n1 = smith_number1.smith_number(N)
     ts1 = time.clock()
-    n2 = os.popen('./smith_number %d' % N).read().strip()
+    command = './smith_number %d' % N
+    _cmd = Popen(command, shell=True, stdout=PIPE, close_fds=True).stdout
+    n2 = _cmd.read().strip()
     ts2 = time.clock()
     ts.append((ts_, n0, ts0, n1, ts1, n2, ts2))
     print n0, n2, n2
