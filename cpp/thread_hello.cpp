d5f3181343f2d8805c28cdb49238531b607d4375
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-28 07:45:46 -0400

    RAII lock class insead of explicit lock/unlock

--- cpp/thread_hello.cpp
+++ cpp/thread_hello.cpp
@@ -3,6 +3,7 @@
 #include <atomic>
 
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <string>
 #include <cstdlib>
@@ -13,14 +14,21 @@ mutex mtx;
 
 atomic<bool> ready(false);
 
+class local_lock {
+    mutex & mtx_;
+public:
+    local_lock(mutex & mtx): mtx_(mtx){ mtx_.lock(); };
+    ~local_lock() { mtx_.unlock(); }
+};
+
 void print_hello(int threadid){
     while(!ready)
         this_thread::yield();
     for(int idx=0; idx<10; idx++){
-        mtx.lock();
-        cout << "Hello there from thread " << threadid << " " << idx << endl;
-        mtx.unlock();
-        this_thread::yield();
+        local_lock current_lock(mtx);
+        stringstream output;
+        output << "Hello there from thread " << threadid << " " << idx << endl;
+        cout << output.str();
     }
 }
 
