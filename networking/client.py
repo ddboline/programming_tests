77ac179656f5bcd50bf88c991320c47751df0d02
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-29 22:55:39 -0400

    ...

--- networking/client.py
+++ networking/client.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import socket
@@ -8,15 +8,15 @@ def main():
     s = socket.socket()
     err = s.connect_ex((os.sys.argv[1], int(os.sys.argv[2])))
     if err:
-        print 'failed to open socket %s' % err
+        print('failed to open socket %s' % err)
         exit(1)
-    s.send(raw_input())
-    print s.recv(1024)
+    s.send(input().encode())
+    print(s.recv(1024))
     s.close()
     return 0
 
 if __name__ == '__main__':
     if len(os.sys.argv) < 3:
-        print "usage %s hostname port\n" % os.sys.argv[0]
+        print("usage %s hostname port\n" % os.sys.argv[0])
         exit(1)
     main()
--- networking/server.py
+++ networking/server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import socket
@@ -15,11 +15,11 @@ def main():
     s.listen(1)
     while 1:
         c, a = s.accept()
-        d = c.recv(1024)
+        d = str(c.recv(1024))
         if not d or d.find('q') >= 0:
             break
-        print "Here is the message: %s" % d
-        c.send(d)
+        print("Here is the message: %s" % d)
+        c.send(d.encode())
         c.close()
     s.shutdown(socket.SHUT_RDWR)
     s.close()
@@ -29,6 +29,6 @@ if __name__ == '__main__':
     try:
         pn = int(os.sys.argv[1])
     except ValueError:
-        print './server.py <port number>'
+        print('./server.py <port number>')
         exit(0)
     main()
