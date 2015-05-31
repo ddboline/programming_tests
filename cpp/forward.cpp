c943b3aca11318dcea6ed9d81f4f5482a447b55e
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-31 09:29:44 -0400

    ...

--- cpp/forward.cpp
+++ cpp/forward.cpp
@@ -14,9 +14,7 @@ public:
     B(T1&& t1, T2&& t2, T3&& t3) :
         a1_{std::forward<T1>(t1)},
         a2_{std::forward<T2>(t2)},
-        a3_{std::forward<T3>(t3)}
-    {
-    }
+        a3_{std::forward<T3>(t3)} {}
  
 private:
     A a1_, a2_, a3_;
@@ -36,6 +34,7 @@ std::unique_ptr<T> make_unique(U&&... u)
  
 int main()
 {   
+    std::cout << "A\n";
     auto p1 = make_unique1<A>(2); // rvalue
     int i = 1;
     auto p2 = make_unique1<A>(i); // lvalue
