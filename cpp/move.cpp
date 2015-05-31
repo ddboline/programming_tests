c85beb35c7c1b9624c986d1cb8f1876de16d44d8
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-31 17:50:17 -0400

    new

---
+++ cpp/move.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <string>
+
+#include <list>
+#include <iterator>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+void f(int n)
+{
+    this_thread::sleep_for(chrono::seconds(n));
+    cout << "thread " << n << " ended" << '\n';
+}
+ 
+int main()
+{
+    string str = "Hello";
+    vector<string> v;
+ 
+    // uses the push_back(const T&) overload, which means 
+    // we'll incur the cost of copying str
+    v.push_back(str);
+    cout << "After copy, str is \"" << str << "\"\n";
+ 
+    // uses the rvalue reference push_back(T&&) overload, 
+    // which means no strings will be copied; instead, the contents
+    // of str will be moved into the vector.  This is less
+    // expensive, but also means str might now be empty.
+    v.push_back(move(str));
+    cout << "After move, str is \"" << str << "\"\n";
+ 
+    cout << "The contents of the vector are \"" << v[0]
+                                         << "\", \"" << v[1] << "\"\n";
+ 
+    // string move assignment operator is often implemented as swap,
+    // in this case, the moved-from object is NOT empty
+    string str2 = "Good-bye";
+    cout << "Before move from str2, str2 = '" << str2 << "'\n";
+    v[0] = move(str2);
+    cout << "After move from str2, str2 = '" << str2 << "'\n";
+
+    
+    vector<thread> v_t;
+    v_t.emplace_back(f, 1);
+    v_t.emplace_back(f, 2);
+    v_t.emplace_back(f, 3);
+    list<thread> l;
+    // copy() would not compile, because thread is noncopyable
+ 
+    move(v_t.begin(), v_t.end(), back_inserter(l)); 
+    for (auto& t : l) t.join();
+    return 0;
+}
