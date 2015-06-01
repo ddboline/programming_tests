db32f0a8320efff490ab62da4a3e0a701d5c52c0
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-31 23:53:08 -0400

    test cpp_socket

--- networking/test_cloud9.sh
+++ networking/test_cloud9.sh
@@ -4,7 +4,7 @@ PORT=`shuf -i 2000-65000 -n 1`
 ./server.py $PORT &
 PROC=$!
 sleep 5
-echo "HEY THERE" | ./client.py localhost $PORT
+./client.py localhost $PORT "HEY THERE"
 
 PORT=`shuf -i 2000-65000 -n 1`
 ./server $PORT &
@@ -12,14 +12,19 @@ PROC="${PROC} $!"
 sleep 5
 echo "HOW GOES IT" | ./client.py localhost $PORT
 
-./threadadd
-
 PORT=`shuf -i 2000-65000 -n 1`
 ./server_udp $PORT &
 PROC="${PROC} $!"
 sleep 5
 ./client_udp localhost $PORT &
 PROC="${PROC} $!"
+
+cd cpp_socket
+PORT=`shuf -i 2000-65000 -n 1`
+./simple_server $PORT &
+PROC="${PROC} $!"
+sleep 5
+./simple_client $PORT "WHAT UP"
 
 sleep 10
 kill -9 ${PROC}
