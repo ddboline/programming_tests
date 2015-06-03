e8f468a87cb2cd22966bdd35951c75e8df1f5d73
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-03 11:31:08 -0400

    OpenUnixSocketClient/OpenUnixSocketServer/OpenSocketConnection

--- networking/client.py
+++ networking/client.py
@@ -1,24 +1,55 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import socket
 import os
+import socket
+
+class OpenUnixSocketClient(object):
+    def __init__(self, host=None, portno=None, socketfile=None):
+        self.sock = None
+        self.socketfile = None
+        self.host = host
+        self.portno = portno
+        if socketfile:
+            self.socketfile = socketfile
+    
+    def __enter__(self):
+        stm_type = socket.SOCK_STREAM
+        if self.socketfile:
+            net_type = socket.AF_UNIX
+            addr_obj = self.socketfile
+        else:
+            net_type = socket.AF_INET
+            addr_obj = (self.host, self.portno)
+        self.sock = socket.socket(net_type, stm_type)
+        try:
+            err = self.sock.connect(addr_obj)
+        except socket.error:
+            return None
+        if err:
+            print(err)
+        return self.sock
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.sock.close()
+        if exc_type or exc_value or traceback:
+            return False
+        else:
+            return True
+
 
 def main():
-    s = socket.socket()
-    err = s.connect_ex((os.sys.argv[1], int(os.sys.argv[2])))
-    if err:
-        print('failed to open socket %s' % err)
-        exit(1)
-    msg = ' '.join(os.sys.argv[3:])
-    s.send(msg.encode())
-    msg = s.recv(1024).decode()
-    print(msg)
-    s.close()
-    return 0
-
-if __name__ == '__main__':
     if len(os.sys.argv) < 3:
         print("usage %s hostname port\n" % os.sys.argv[0])
         exit(1)
+    host = os.sys.argv[1]
+    port = int(os.sys.argv[2])
+    with OpenUnixSocketClient(host, port) as sock:
+        msg = ' '.join(os.sys.argv[3:])
+        sock.send(msg.encode())
+        msg = sock.recv(1024).decode()
+        print(msg)
+    return 0
+
+if __name__ == '__main__':
     main()
--- networking/server.py
+++ networking/server.py
@@ -1,34 +1,81 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import socket
 import os
+import time
+import socket
+
+class OpenUnixSocketServer(object):
+    def __init__(self, socketfile=None, host=None, portno=None):
+        self.sock = None
+        self.socketfile = socketfile
+        self.host = host
+        self.portno = portno
+        if socketfile and os.path.exists(socketfile):
+            os.remove(socketfile)
+        return
+
+    def __enter__(self):
+        if self.socketfile:
+            net_type = socket.AF_UNIX
+            addr_obj = self.socketfile
+        else:
+            net_type = socket.AF_INET
+            addr_obj = (self.host, self.portno)
+        self.sock = socket.socket(net_type, socket.SOCK_STREAM)
+        try:
+            self.sock.bind(addr_obj)
+            if self.socketfile:
+                os.chmod(self.socketfile, 0o777)
+        except:
+            print('failed to open socket')
+            time.sleep(10)
+            return self.__enter__()
+        print('open socket')
+        self.sock.listen(0)
+        return self.sock
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.sock.shutdown(socket.SHUT_RDWR)
+        self.sock.close()
+        if exc_type or exc_value or traceback:
+            return False
+        else:
+            return True
+
+
+class OpenSocketConnection(object):
+    def __init__(self, sock):
+        self.sock = sock
+
+    def __enter__(self):
+        self.conn, _ = self.sock.accept()
+        return self.conn
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.close()
+        if exc_type or exc_value or traceback:
+            return False
+        else:
+            return True
+
 
 def main():
-    portno = int(os.sys.argv[1])
-    host = 'localhost'
-    s = socket.socket()
     try:
-        s.bind((host, portno))
-    except socket.error:
-        return
-    s.listen(1)
-    while 1:
-        c, a = s.accept()
-        d = c.recv(1024).decode()
-        if not d or d.find('q') >= 0:
-            break
-        print("Here is the message: %s" % d)
-        c.send(d.encode())
-        c.close()
-    s.shutdown(socket.SHUT_RDWR)
-    s.close()
-    return 0
-
-if __name__ == '__main__':
-    try:
-        pn = int(os.sys.argv[1])
+        portno = int(os.sys.argv[1])
     except ValueError:
         print('./server.py <port number>')
         exit(0)
+    host = 'localhost'
+    with OpenUnixSocketServer(host=host, portno=portno) as sock:
+        while 1:
+            with OpenSocketConnection(sock) as conn:
+                dump = conn.recv(1024).decode()
+                if not dump or dump.find('q') >= 0:
+                    break
+                print("Here is the message: %s" % dump)
+                conn.send(dump.encode())
+    return 0
+
+if __name__ == '__main__':
     main()
