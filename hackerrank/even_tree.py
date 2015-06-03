35bf3697b29400762f1c2860a981559d74eae697
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-03 19:10:29 -0400

    ...

--- hackerrank/even_tree.py
+++ hackerrank/even_tree.py
@@ -1,46 +1,29 @@
-#!/bin/python
+#!/usr/bin/python3
 
 def even_tree(N, edges):
-    list_of_trees = [edges]
-    new_list_of_trees = []
-    
-    verticies = {n: [] for n in range(1,N+1)}
-    for n in range(len(edges)):
-        e0 = edges[n][0]
-        e1 = edges[n][1]
-        if e1 not in verticies[e0]:
-            verticies[e0].append(e1)
-        if e0 not in verticies[e1]:
-            verticies[e1].append(e0)
-    
-    print verticies
-        
-    for n in range(len(edges)):
-        e0 = edges[n][0]
-        e1 = edges[n][1]
-        
-        if len(verticies[e0]) == 1 or len(verticies[e1]) == 1:
-            continue
-        
-        test_trees = []
-        
-        print e0, verticies[e0] , [verticies[e] for e in verticies[e0] if e0 not in verticies[e] ]
-        
-        print e1, verticies[e1]
-    
-    return list_of_trees
+    edge_dict = {n: set() for n in range(1,N+1)}
+    for u, v in edges:
+        edge_dict[u].add(v)
+        edge_dict[v].add(u)
+    u, v = edges[0]
+    start = u
+    visited = set({u})
+    for e in [x for x in edge_dict[start] if x != v]:
+        visited.add(e)
+    print(visited)
+    return
 
-    
 N, M = 10, 9
-edges = [(2, 1), (3, 1), (4, 3), (5, 2), 
-         (6, 1), (7, 2), (8, 6), (9, 8), (10, 8),]
+edges = ((2, 1), (3, 1), (4, 3), (5, 2), 
+         (6, 1), (7, 2), (8, 6), (9, 8), (10, 8),)
 
 even_tree(N, edges)
 
-print '\n\n'
+print('\n\n')
 
-N, M = 12, 11
-edges = [(2, 1), (3, 1), (4, 3), (5, 2), 
-         (6, 1), (7, 2), (8, 6), (9, 8), (10, 8), (1, 11), (6,12)]
+N, M = 20, 19
+edges = ((2, 1), (3, 1), (4, 3), (5, 2), (6, 5), (7, 1), (8, 1), (9, 2),
+         (10, 7), (11, 10), (12, 3), (13, 7), (14, 8), (15, 12), (16, 6),
+         (17, 6), (18, 10), (19, 1), (20, 8))
 
 even_tree(N, edges)
