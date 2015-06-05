e6c65aa96b91dd5f1825cac83965d81da9710af8
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-05 10:14:32 -0400

    ...

--- hackerrank/even_tree.py
+++ hackerrank/even_tree.py
@@ -12,52 +12,54 @@ def get_vdict(edges):
 
 def walk_tree(vdict, vset, ucurr, vcurr=None):
     if ucurr not in vdict:
-        return
+        return 0
+    vset.add(ucurr)
     if not vdict[ucurr]:
-        return
+        return 0
     for node in vdict[ucurr]:
         if node == vcurr:
             continue
         if node not in vset:
             vset.add(node)
             walk_tree(vdict, vset, node, ucurr)
-    return
+    return 1
 
-def even_tree(edges):
+def split_tree(edges):
     vdict = get_vdict(edges)
     for edge in edges:
         u, v = edge
         vset0 = set()
         walk_tree(vdict, vset0, u, v)
         
+        if len(vset0) == 0 or len(vset0) % 2 != 0:
+            continue
+        
         vset1 = set()
         walk_tree(vdict, vset1, v, u)
         
-        if len(vset0) == 0 or len(vset1) == 0:
-            continue
-        if len(vset0) % 2 != 0 or len(vset1) % 2 != 0:
+        if len(vset1) == 0 or len(vset1) % 2 != 0:
             continue
         print('vset0', vset0, 'vset1', vset1)
+
         edge0 = []
         edge1 = []
         for ed in edges:
-#            if ed == edge:
-#                continue
+            if ed == edge:
+                continue
             u, v = ed
             if u in vset0 and v in vset0:
                 edge0.append(ed)
             elif u in vset1 and v in vset1:
                 edge1.append(ed)
-        print(edge0, edge1)
-        even_tree(edge0)
-        even_tree(edge1)
-    return None
-
+        return split_tree(edge0) + split_tree(edge1)
+        
+    return edges
+        
 
 N, M = 10, 9
 edges = ((2, 1), (3, 1), (4, 3), (5, 2), 
          (6, 1), (7, 2), (8, 6), (9, 8), (10, 8),)
-print(even_tree(edges))
+print('even_tree', len(edges) - len(split_tree(edges)))
 
 print('\n\n')
 
@@ -66,4 +68,4 @@ edges = ((2, 1), (3, 1), (4, 3), (5, 2), (6, 5), (7, 1), (8, 1), (9, 2),
          (10, 7), (11, 10), (12, 3), (13, 7), (14, 8), (15, 12), (16, 6),
          (17, 6), (18, 10), (19, 1), (20, 8))
 
-print(even_tree(edges))
+print('even_tree', len(edges) - len(split_tree(edges)))
