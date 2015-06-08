79655450a96a7885ae7c5f7839b0866a986f7104
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-08 13:38:22 -0400

    ...

---
+++ cython/integ3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from numba import jit
+
+@jit
+def f(x):
+    return x**2 - x
+
+@jit
+def integrate_f(a, b, N):
+    s = 0
+    dx = (b - a) / N
+    i = 0
+    while i < N:
+        s += f(a + i * dx)
+        i += 1
+    return s * dx
