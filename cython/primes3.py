a39d25ddb71216e4fb6e76882c491b3f5d2b16ee
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-08 12:25:35 -0400

    ...

--- cython/primes3.py
+++ cython/primes3.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
-try:
-    from numba import jit
-    from numba import int32
-    def intarray():
-        return int32[:]
-except ImportError:
-    def jit(arg):
-        print('no jit')
-        return arg
-    def intarray():
-        return []
+from numba import jit
+from numba import int32
 
 @jit
 def primes(kmax):  # The argument will be converted to int or raise a TypeError.
     #cdef int n, k, i  # These variables are declared with C types.
     #cdef int p[1000] # Another C type
-    primes = intarray()
+    primes = int32[:]
     largest_prime = 1
     n_primes = 0
     prime_candidate = 2
@@ -24,7 +15,7 @@ def primes(kmax):  # The argument will be converted to int or raise a TypeError.
         while i < n_primes and prime_candidate % primes[i] != 0:
             i += 1
         if i == n_primes:
-            primes.append(prime_candidate)
+            primes[i] = prime_candidate
             n_primes += 1
             largest_prime = prime_candidate
         prime_candidate += 1
