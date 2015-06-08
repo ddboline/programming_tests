3812acba127ea6a07060b3b4f85ae894ff7de8df
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-08 13:48:49 -0400

    ...

---
+++ cython/pi_wallis2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from numba import jit
+import os
+
+@jit
+def pi_wallis(n=10):
+    val = 2.
+    for i in range(1, n):
+        val *= (4.*i**2) / (4.*i**2 - 1.)
+    return val
--- cython/run_pi_wallis.py
+++ cython/run_pi_wallis.py
@@ -5,6 +5,7 @@ pyximport.install()
 
 import pi_wallis0
 import pi_wallis1
+import pi_wallis2
 
 import time
 import math
@@ -17,5 +18,7 @@ print(pi_wallis0.pi_wallis(N))
 t1 = time.clock()
 print(pi_wallis1.pi_wallis(N))
 t2 = time.clock()
+print(pi_wallis2.pi_wallis(N))
+t3 = time.clock()
 
-print('timing:', t1-t0, t2-t1)
+print('timing:', t1-t0, t2-t1, t3-t2)
