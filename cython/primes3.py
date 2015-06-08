3fc6e2dbc090bd19f5054c9543e7296bef4899e6
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-08 13:07:34 -0400

    ...

--- cython/primes3.py
+++ cython/primes3.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-import numpy as np
-#from numba import jit, int64
+from numba import jit, int64
 
-#@jit
+@jit
 def primes(kmax):  # The argument will be converted to int or raise a TypeError.
     #cdef int n, k, i  # These variables are declared with C types.
     #cdef int p[1000] # Another C type
-    primes_ = np.zeros(1000, dtype=np.int64)
+    primes_ = 1000*[0]
     primes_size = 1000
     largest_prime = 1
     n_primes = 0
@@ -19,8 +18,7 @@ def primes(kmax):  # The argument will be converted to int or raise a TypeError.
             try:
                 primes_[i] = prime_candidate
             except IndexError:
-                primes_ = np.append(primes_, np.zeros(primes_size, dtype=np.int64))
-                primes_size = primes_.size
+                primes_.extend(1000*[0])
                 primes_[i] = prime_candidate
             n_primes += 1
             largest_prime = prime_candidate
