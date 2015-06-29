27016d440211c97353673f9d849a01716992360b
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-28 20:19:00 -0400

    play with arrays

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -5,6 +5,14 @@ public class HelloWorld {
         this_student.setName("HARRY");
         Student other_student = new Student("SALLY");
         System.out.printf("%s %s\n", this_student.getName(), other_student.getName());
+        
+        int[] intarray = new int[10];
+        for(int i=0; i<10; i++){
+            intarray[i] = 3*i+2;
+        }
+        for(int i=0; i<10; i++){
+            System.out.printf("%d %d\n", i, intarray[i]);
+        }
     }
     public static int PrintHelloWorld() {
         System.out.println("Hello World!");
