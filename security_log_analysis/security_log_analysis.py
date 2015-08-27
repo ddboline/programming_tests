c89abe8859c9c04ed9b4ad35ffb0268f30d6a0dd
Daniel Boline <ddboline@gmail.com>
2015-08-27 16:36:02 -0400

    ...

--- security_log_analysis/security_log_analysis.py
+++ security_log_analysis/security_log_analysis.py
@@ -44,37 +44,20 @@ def plot_time_access(csvfile, title):
     plt.savefig('%s_weekday.png' % title, format='png')
     plt.clf()
 
-def fill_country_plot():
-    fname_ssh = 'logcsv.csv.gz'
-    fname_http = 'logcsv_apache.csv.gz'
+def fill_country_plot(engine):
+    table = 'country_count'
     outfname = 'ssh_intrusion_attempts.html'
     if HOSTNAME != 'dilepton-tower':
-        fname_ssh = 'logcsv_cloud.csv.gz'
-        fname_http = 'logcsv_apache_cloud.csv.gz'
+        table = 'country_count_cloud'
         outfname = 'ssh_intrusion_attempts_cloud.html'
-    ssh_df = pd.read_csv(fname_ssh, compression='gzip')
-    apache_df = pd.read_csv(fname_http, compression='gzip')
-    ccode_df = pd.read_csv('country_code_name.csv.gz', compression='gzip')
-    host_country_df = pd.read_csv('host_country.csv.gz', compression='gzip')
 
-    print([x.columns for x in (ssh_df, ccode_df, host_country_df)])
-    print([x.shape for x in (ssh_df, ccode_df, host_country_df)])
-
-    ssh_country_df = pd.concat([ssh_df, apache_df])
-    ssh_country_df = pd.merge(ssh_country_df, host_country_df, on='Host')
-    ssh_country_df = pd.merge(ssh_country_df, ccode_df, on='Code')
-
-    uniq_host_counts = ssh_country_df['Country'].unique()
-    uniq_country_counts = ssh_country_df['Country'].value_counts()
-
-    print(uniq_host_counts)
-    print(uniq_country_counts)
-
+    
     with open(outfname, 'w') as output:
         with open('COUNTRY_TEMPLATE.html', 'r') as inpfile:
             for line in inpfile:
                 if 'PUTLISTOFCOUNTRIESANDATTEMPTSHERE' in line:
-                    for c, n in uniq_country_counts.to_dict().items():
+                    cmd = 'select * from %s;' % table
+                    for c, n in engine.execute(cmd):
                         output.write("%10s['%s', %d],\n" % ('', c, n))
                 else:
                     output.write(line)
@@ -86,15 +69,14 @@ def fill_country_plot():
 if __name__ == '__main__':
 #    dump_sql_csv()
 
-    plot_time_access('logcsv.csv.gz', 'ssh_access')
+#    plot_time_access('logcsv.csv.gz', 'ssh_access')
     #plot_time_access('logcsv_cloud.csv.gz', 'ssh_access_cloud')
     #plot_time_access('logcsv_apache.csv.gz', 'apache_access')
     #plot_time_access('logcsv_apache_cloud.csv.gz', 'apache_access_cloud')
 
-    fill_country_plot()
-
     with OpenPostgreSQLsshTunnel():
         engine = create_db_engine()
+        fill_country_plot(engine)
         cmd = "select * from local_remote_compare"
         for line in engine.execute(cmd):
             print(line)
