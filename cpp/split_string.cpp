21dc3fc8f5bcf4bb355dc3a9f234b83062e4c55e
Daniel Boline <ddboline@gmail.com>
2015-09-10 13:39:20 -0400

    catch exception, output as param, print_strings function

--- cpp/split_string.cpp
+++ cpp/split_string.cpp
@@ -9,13 +9,18 @@ using namespace std;
 
 vector<int> string_to_numbers(const vector<string> & output){
     vector<int> numbers;
-    for(auto it=output.begin(); it!=output.end(); ++it)
-        numbers.push_back(stoi(*it));
+    for(auto it=output.begin(); it!=output.end(); ++it){
+        try {
+            numbers.push_back(stoi(*it));
+        }
+        catch(invalid_argument){
+            continue;
+        }
+    }
     return numbers;
 }
 
-int split_string_manual(const string & input, const string & delim){
-    vector<string> output;
+int split_string_manual(const string & input, vector<string> & output, const string & delim){
     string current = "";
     for(auto it=input.begin(); it!=input.end(); ++it){
         auto chr = (*it);
@@ -27,27 +32,37 @@ int split_string_manual(const string & input, const string & delim){
             current += chr;
     }
     output.push_back(current);
-    vector<int> numbers = string_to_numbers(output);
-    for(auto it=numbers.begin(); it!=numbers.end(); ++it)
-        cout << (*it) << " ";
-    cout << endl;
     return 0;
 }
 
-int split_string_boost(const string & input, const string & delim){
-    vector<string> output;
+int split_string_boost(const string & input, vector<string> & output, const string & delim){
     boost::algorithm::split(output, input, boost::is_any_of(","));
-    vector<int> numbers = string_to_numbers(output);
+    return 0;
+}
+
+void print_strings(vector<string> & input){
+    vector<int> numbers = string_to_numbers(input);
     for(auto it=numbers.begin(); it!=numbers.end(); ++it)
         cout << (*it) << " ";
     cout << endl;
-    return 0;
 }
 
 int main(int argc, char ** argv){
     string delim = ",";
     string input = "0,1234,5,1234";
-    split_string_manual(input, delim);
-    split_string_boost(input, delim);
+    vector<string> output;
+    split_string_manual(input, output, delim);
+    print_strings(output);
+    output.resize(0);
+    split_string_boost(input, output, delim);
+    print_strings(output);
+
+    for(int i=1; i<argc; i++){
+        string input(argv[i]);
+        output.resize(0);
+        split_string_manual(input, output, delim);
+        print_strings(output);
+    }
+    
     return 0;
 }
