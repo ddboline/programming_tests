28dd73f3459a943b954989fb3e020935544d90cd
Daniel Boline <ddboline@gmail.com>
2015-10-05 17:01:14 -0400

    fixes

--- cython/setup.py
+++ cython/setup.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python
 
 import os
+import sys
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
+PY23 = 'python%d.%d' % (sys.version_info[0:2])
+INCDIR = '/usr/lib/%s/numpy' % PY23
+if not os.path.exists(INCDIR):
+    INCDIR = '/usr/lib/%s/dist-packages/numpy/core/include' % PY23
+
 extensions = [
-    Extension("matmul2", ["matmul2.pyx"],
-        include_dirs=['/usr/lib/python2.7/dist-packages/numpy/core/include']),
+    Extension("matmul2", ["matmul2.pyx"], 
+        include_dirs=[INCDIR]),
     Extension("matmul3", ["matmul3.pyx"],
-        include_dirs=['/usr/include/gsl/', 
-                      '/usr/lib/python2.7/dist-packages/numpy/core/include'],
+        include_dirs=['/usr/include/gsl/', INCDIR],
         libraries=['gslcblas'],
         library_dirs=['/usr/lib']),
-    Extension('cos_func', ['cos_func.pyx'],
-                 libraries=['m']),
+    Extension('cos_func', ['cos_func.pyx'], libraries=['m']),
     Extension('sampling_vose_alias_method1',
               ['sampling_vose_alias_method1.pyx'],
               language="c++",),
