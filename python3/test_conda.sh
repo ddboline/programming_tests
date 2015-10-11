b6925a21f69c5c21a57001fc57c6a0fe1acb0d42
Daniel Boline <ddboline@gmail.com>
2015-10-11 04:38:34 -0400

    ...

---
+++ python3/test_conda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+nosetests unittests.py analyze_gmail.py file_sync.py exversion.py parse_email.py util.py
+
+./analyze_gmail.py temp.mbox
+./default_dict_tree.py
+./file_sync.py
+# ./stock_parser.py
+./sampling_vose_alias_method.py
