e6a2825c56b341d38af3e2fb32840cc5f79bbb61
Daniel Boline <ddboline@gmail.com>
2016-01-28 08:04:23 -0500

    replace with shorter string based version

--- cpp/remove_char_inplace.cpp
+++ cpp/remove_char_inplace.cpp
@@ -6,27 +6,18 @@
 
 using namespace std;
 
-void removeChar(char * source, int index){
-        // creates another char* with the same pointer as source
-        char* scopy = &source[0];
-        int len = strlen(source);
- 
-        // loops through the string, keeping its contents until index is found.
-        for (int i =0; i< len; i++) {
- 
-                // checks if the current char doesnt match index char, current char to string
-                if (i != index) {
-                        *scopy = source[i];
-                        scopy++;
-                }   
+void removeChar(string & source, unsigned int index) {
+    for(unsigned int i=0; i<source.size(); i++){
+        if(i >= index){
+            source[i] = source[i+1];
         }
-        // end new string
-        *scopy = '\0';
+    }
 }
 
 int main(int argc, char ** argv){
     if(argc > 1){
-        char * a = argv[1];
+//         char * a = argv[1];
+        string a(argv[1]);
         removeChar(a, 2);
         cout << a << " " << endl;
     }
