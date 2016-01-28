dda2de7d9c46ad2cb8bc17fe66ddab0981d8c517
Daniel Boline <ddboline@gmail.com>
2016-01-28 08:01:28 -0500

    shared_ptr

--- cpp/doubly_linked_list.cpp
+++ cpp/doubly_linked_list.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
 struct node {
     int x;
-    node *prev;
-    node *next;
+    shared_ptr<node> prev;
+    shared_ptr<node> next;
 };
 
 class doubly_linked_list {
 public:
-    doubly_linked_list() {
-        root_node = new node;
+    doubly_linked_list() :
+        root_node(new node)
+    {
         root_node->x = 0;
         root_node->prev = nullptr;
         root_node->next = nullptr;
@@ -21,15 +23,9 @@ public:
         root_node = x.root_node;
         current_node = x.current_node;
     };
-    ~doubly_linked_list() {
-        while(root_node) {
-            cout << "remove " << root_node->x << " ";
-            remove_front();
-        }
-        cout << endl;
-    }
+    ~doubly_linked_list() {};
     void add(int x) {
-        node * new_node = new node;
+        shared_ptr<node> new_node(new node);
         new_node->x = x;
         new_node->prev = current_node;
         new_node->next = nullptr;
@@ -37,35 +33,33 @@ public:
         current_node = new_node;
     }
     void remove_back() {
-        node * temp = current_node;
+        shared_ptr<node> temp(current_node);
         current_node = current_node->prev;
-        delete temp;
     }
     void remove_front() {
-        node * temp = root_node;
+        shared_ptr<node> temp(root_node);
         root_node = root_node->next;
-        delete temp;
     }
     void print_list() {
-        node * temp = root_node;
+        shared_ptr<node> temp(root_node);
         while(temp) {
             cout << temp->x << " ";
             temp = temp->next;
         }
         cout << endl;
     }
-    node * begin() {return root_node;};
-    node * end() {return current_node;};
+    shared_ptr<node> begin() {return root_node;};
+    shared_ptr<node> end() {return current_node;};
 private:
-    node * root_node;
-    node * current_node;
+    shared_ptr<node> root_node;
+    shared_ptr<node> current_node;
 };
 
-node * add_to_list(node * current_node, int value) {
+shared_ptr<node> add_to_list(const shared_ptr<node> & current_node, int value) {
     if(!current_node)
         return current_node;
-    node * new_node = new node;
-    node * temp_node = current_node->next;
+    shared_ptr<node> new_node(new node);
+    shared_ptr<node> temp_node = current_node->next;
     current_node->next = new_node;
     if(temp_node) {
         temp_node->prev = new_node;
@@ -88,10 +82,10 @@ int recurse(int n){
 };
 
 int main() {
-    node *root = new node;
+    shared_ptr<node> root(new node);
     root->next = nullptr;
     root->x = 5;
-    node *current_node = root;
+    shared_ptr<node> current_node(root);
     for(int i = 0; i<100; i++)
         current_node = add_to_list(current_node, i + 12);
     current_node = root;
