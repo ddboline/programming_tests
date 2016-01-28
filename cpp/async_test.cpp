58c4c4d67b33e6432da640a595d7306e94a1c23b
Daniel Boline <ddboline@gmail.com>
2016-01-28 08:01:12 -0500

    std::

--- cpp/async_test.cpp
+++ cpp/async_test.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
+#include <string>
 #include <future>
  
 #define CYCLES 100
- 
-void test(const char* name) {
-    int i;
-    for (i = 0; i < CYCLES; i++) {
-            std::cout << "Hello world (" << name << ") " << (i + 1) << std::endl;
+
+using namespace std;
+
+void test(const string & name) {
+    for (int i = 0; i < CYCLES; i++) {
+            cout << "Hello world (" << name << ") " << (i + 1) << endl;
     }
 }
  
 int main () {
-    const char* t1 = "GODZILLA";
-    const char* t2 = "DRACULA";
-    const char* t3 = "GRU";
+    string t1("GODZILLA");
+    string t2("DRACULA");
+    string t3("GRU");
 
-    auto async_task1 = std::async(std::launch::async, test, t1);
-    auto async_task2 = std::async(std::launch::async, test, t2);
-    auto async_task3 = std::async(std::launch::async, test, t3);
+    auto async_task1 = async(launch::async, test, t1);
+    auto async_task2 = async(launch::async, test, t2);
+    auto async_task3 = async(launch::async, test, t3);
 
     async_task1.get();
     async_task2.get();
