48c51a3e8fc46c3b9a28d9be8932950614555fa9
Daniel Boline <ddboline@gmail.com>
2017-01-08 11:49:13 -0500

    nose -> pytest

--- python/test_cloud9.sh
+++ python/test_cloud9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nosetests unittests.py analyze_gmail.py file_sync.py exversion.py parse_email.py util.py
+py.test unittests.py analyze_gmail.py file_sync.py exversion.py parse_email.py util.py
 
 ./analyze_gmail.py temp.mbox
 ./default_dict_tree.py
