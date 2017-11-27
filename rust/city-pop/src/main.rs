1d33e4d5b499aed04fcf14aede06cafd09b4591a
Daniel Boline <danielb@visibleworld.com>
2017-11-27 15:06:07 -0500

    ...

--- rust/city-pop/src/main.rs
+++ rust/city-pop/src/main.rs
@@ -28,7 +28,10 @@ struct Row {
 }
 
 fn print_usage(program: &str, opts: Options) {
-    println!("{}", opts.usage(&format!("Usage: {} [options] <city>", program)));
+    println!(
+        "{}",
+        opts.usage(&format!("Usage: {} [options] <city>", program))
+    );
 }
 
 fn search<P: AsRef<Path>>(file_path: &Option<P>, city: &str) {
@@ -37,20 +40,23 @@ fn search<P: AsRef<Path>>(file_path: &Option<P>, city: &str) {
         Some(ref file_path) => Box::new(File::open(file_path).unwrap()),
     };
 
-//     let file = File::open(file_path).unwrap();
-//     let d = GzDecoder::new(&file).unwrap();
+    //     let file = File::open(file_path).unwrap();
+    //     let d = GzDecoder::new(&file).unwrap();
     let mut rdr = csv::Reader::from_reader(input);
     let mut sum = 0;
 
-    let total = rdr.decode::<Row>().filter(|r| r.is_ok())
-                                   .map(|r| r.unwrap())
-                                   .filter(|r| r.population.is_some())
-                                   .filter(|r| r.city.contains(city))
-                                   .map(|r| {
-                                    let pop = r.population.unwrap();
-                                    println!("{}, {}: {}", r.city, r.country, pop);
-                                    sum += pop;
-                                    r}).count();
+    let total = rdr.decode::<Row>()
+        .filter(|r| r.is_ok())
+        .map(|r| r.unwrap())
+        .filter(|r| r.population.is_some())
+        .filter(|r| r.city.contains(city))
+        .map(|r| {
+            let pop = r.population.unwrap();
+            println!("{}, {}: {}", r.city, r.country, pop);
+            sum += pop;
+            r
+        })
+        .count();
 
     println!("{} {}", total, sum);
 }
@@ -64,15 +70,15 @@ fn main() {
     opts.optflag("h", "help", "Show this usage message.");
 
     let matches = match opts.parse(&args[1..]) {
-        Ok(m)  => { m }
-        Err(e) => { panic!(e.to_string()) }
+        Ok(m) => m,
+        Err(e) => panic!(e.to_string()),
     };
     if matches.opt_present("h") {
         print_usage(&program, opts);
         return;
     }
     let data_path = matches.opt_str("f");
-    
+
     let city = if !matches.free.is_empty() {
         &matches.free[0]
     } else {
