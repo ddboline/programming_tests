65821d4d24d0c2d930f0d385d68e1926703f8283
Daniel Boline <danielb@visibleworld.com>
2018-05-15 17:31:30 -0400

    ...:

--- rust/project_euler/src/main.rs
+++ rust/project_euler/src/main.rs
@@ -16,9 +16,17 @@ use collatz_chain::find_longest_chain;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let mut do_all = false;
+    let mut do_sumprime = false;
+    let mut do_triangle = false;
     for arg in args {
         if arg.contains("all") {
             do_all = true
+        }
+        if arg.contains("sumprime") {
+            do_sumprime = true
+        }
+        if arg.contains("triangle") {
+            do_triangle = true
         }
     }
 
@@ -42,6 +50,11 @@ fn main() {
             triangle_with_n_divisors(5)
         );
         println!("large_sum {}", large_sum());
+    } else if do_sumprime {
+        println!("sum_of_primes(2000000) 142913828922 {}", sum_of_primes(2000000));
+    } else if do_triangle {
+        println!("triangle_with_n_divisors(500) 76576500 {}", triangle_with_n_divisors(500));
     }
+    
     println!("find_longest_chain(1000) {}", find_longest_chain(1000));
 }
