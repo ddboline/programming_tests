1708f6723daca0702dccca8c60c0e6556c5a05f5
Daniel Boline <ddboline@gmail.com>
2019-06-19 06:43:08 -0400

    ...

--- postgresql/backup_postgres.sh
+++ postgresql/backup_postgres.sh
@@ -3,15 +3,15 @@
 DATABASES="
 florida_doc_inmate_info
 garmin_summary
-lahman2014
 movie_queue
 mydb
 podcatch
 postgres
-quassel
 recipes
 ssh_intrusion_logs
 texas_high_value_datasets
+rust_auth_server
+sync_app_cache
 "
 
 for DB in $DATABASES;
