1dd4257f0dbbf8f6d99251f92982a93ed87bce0b
Daniel Boline <ddboline@gmail.com>
2020-03-05 07:41:31 -0500

    cleanup a couple things

--- rust/topological_sort/src/main.rs
+++ rust/topological_sort/src/main.rs
@@ -1,9 +1,11 @@
 use anyhow::{format_err, Error};
-use std::collections::HashSet;
-use std::fmt::Debug;
 use log::debug;
+use serde::{Deserialize, Serialize};
+use std::collections::{BTreeMap, BTreeSet, VecDeque};
+use std::fmt::Debug;
+use std::mem::replace;
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub struct NodeIndex(usize);
 
 impl NodeIndex {
@@ -20,20 +22,42 @@ impl From<usize> for NodeIndex {
 
 #[derive(Debug)]
 pub struct Node<T: Debug> {
-    data: T,
-    children: Vec<NodeIndex>,
+    data: Option<T>,
+    children: BTreeSet<NodeIndex>,
+    parents: BTreeSet<NodeIndex>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NodeSerialize {
+    children: BTreeSet<NodeIndex>,
+    parent: BTreeSet<NodeIndex>,
 }
 
 impl<T: Debug> Node<T> {
     pub fn new(data: T) -> Self {
         Self {
-            data,
-            children: Vec::new(),
+            data: Some(data),
+            children: BTreeSet::new(),
+            parents: BTreeSet::new(),
         }
     }
 
-    pub fn get_children(&self) -> &[NodeIndex] {
+    pub fn get_children(&self) -> &BTreeSet<NodeIndex> {
         &self.children
+    }
+
+    pub fn get_parents(&self) -> &BTreeSet<NodeIndex> {
+        &self.parents
+    }
+}
+
+impl<T: Debug> Default for Node<T> {
+    fn default() -> Self {
+        Self {
+            data: None,
+            children: BTreeSet::new(),
+            parents: BTreeSet::new(),
+        }
     }
 }
 
@@ -43,150 +67,199 @@ pub enum IndexOrData<T> {
 }
 
 #[derive(Debug)]
-pub struct GraphVariable<T: Debug> {
+pub struct Graph<T: Debug> {
     nodes: Vec<Node<T>>,
+    occupied: BTreeSet<NodeIndex>,
+    free: Vec<NodeIndex>,
 }
 
-impl<T: Debug> Default for GraphVariable<T> {
+impl<T: Debug> Default for Graph<T> {
     fn default() -> Self {
-        Self { nodes: Vec::new() }
+        Self {
+            nodes: Vec::new(),
+            occupied: BTreeSet::new(),
+            free: Vec::new(),
+        }
     }
 }
 
-impl<T: Debug + Eq> GraphVariable<T> {
+impl<T: Debug + Eq> Graph<T> {
     pub fn get_node_by_data(&self, data: &T) -> Option<NodeIndex> {
-        for (idx, node) in self.nodes.iter().enumerate() {
-            if &node.data == data {
-                return Some(idx.into());
+        for idx in &self.occupied {
+            let node_data = &self.nodes[idx.idx()];
+            if node_data.data.as_ref().unwrap() == data {
+                return Some(*idx);
             }
         }
         None
     }
 }
 
-impl<T: Debug> GraphVariable<T> {
+impl<T: Debug> Graph<T> {
     pub fn new() -> Self {
         Self::default()
     }
 
-    pub fn get_nodes(&self) -> &[Node<T>] {
-        &self.nodes
+    pub fn get_occupied_nodes(&self) -> &BTreeSet<NodeIndex> {
+        &self.occupied
     }
 
     pub fn get_node_by_index(&self, idx: NodeIndex) -> Option<&Node<T>> {
         self.nodes.get(idx.idx())
     }
 
-    /// Add child to existing node
-    pub fn push<U>(&mut self, parent_index: U, index_or_data: IndexOrData<T>) -> Option<NodeIndex>
+    fn create_child_in_first_available(&mut self, data: T) -> NodeIndex {
+        let child = Node::new(data);
+        if let Some(index) = self.free.pop() {
+            let node = self.nodes.get_mut(index.idx()).unwrap();
+            replace(node, child);
+            self.occupied.insert(index);
+            index
+        } else {
+            let index = self.nodes.len().into();
+            self.nodes.push(child);
+            self.occupied.insert(index);
+            index
+        }
+    }
+
+    pub fn push_index<U>(&mut self, parent_index: U, child_index: NodeIndex) -> Option<NodeIndex>
     where
-        U: Into<Option<NodeIndex>>,
+        U: Into<NodeIndex>,
     {
-        let parent_index: NodeIndex = match parent_index.into() {
-            Some(p) => p,
-            None => {
-                if let IndexOrData::Data(data) = index_or_data {
-                    let child = Node::new(data);
-                    let index = self.nodes.len().into();
-                    self.nodes.push(child);
-                    return Some(index);
-                } else {
-                    return None;
-                }
-            }
-        };
+        let parent_index = parent_index.into();
         if parent_index.idx() < self.nodes.len() {
-            let child_index = match index_or_data {
-                IndexOrData::Index(index) => index,
-                IndexOrData::Data(data) => {
-                    let child = Node::new(data);
-                    let index = self.nodes.len().into();
-                    self.nodes.push(child);
-                    index
-                }
-            };
-            let parent = unsafe { self.nodes.get_unchecked_mut(parent_index.idx()) };
-            parent.children.push(child_index);
+            let parent = self.nodes.get_mut(parent_index.idx()).unwrap();
+            parent.children.insert(child_index);
+            let child = self.nodes.get_mut(child_index.idx()).unwrap();
+            child.parents.insert(parent_index);
             Some(child_index)
         } else {
             None
         }
     }
+
+    /// Add child to existing node
+    pub fn push_child<U>(&mut self, parent_index: U, data: T) -> Option<NodeIndex>
+    where
+        U: Into<Option<NodeIndex>>,
+    {
+        let parent_index: NodeIndex = if let Some(p) = parent_index.into() {
+            p
+        } else {
+            return Some(self.create_child_in_first_available(data));
+        };
+        if parent_index.idx() < self.nodes.len() {
+            let child_index = self.create_child_in_first_available(data);
+            let parent = self.nodes.get_mut(parent_index.idx()).unwrap();
+            parent.children.insert(child_index);
+            let child = self.nodes.get_mut(child_index.idx()).unwrap();
+            child.parents.insert(parent_index);
+            Some(child_index)
+        } else {
+            None
+        }
+    }
+
+    pub fn pop(&mut self, index: NodeIndex) -> Option<Node<T>> {
+        if !self.occupied.contains(&index) {
+            return None;
+        }
+        let empty_node = Node::default();
+        let node = self.nodes.get_mut(index.idx()).unwrap();
+        let node = replace(node, empty_node);
+
+        for parent in &node.parents {
+            let parent_node = self.nodes.get_mut(parent.idx()).unwrap();
+            parent_node.children.remove(&index);
+            for child in &node.children {
+                parent_node.children.insert(*child);
+            }
+        }
+        for child in &node.children {
+            let child_node = self.nodes.get_mut(child.idx()).unwrap();
+            child_node.parents.remove(&index);
+            for parent in &node.parents {
+                child_node.parents.insert(*parent);
+            }
+        }
+        self.occupied.remove(&index);
+        self.free.push(index);
+        Some(node)
+    }
 }
 
-pub fn kahn_algorithm<T: Debug>(dag: &GraphVariable<T>) -> Result<Vec<NodeIndex>, Error> {
-    let mut inverse_graph: Vec<_> = (0..dag.get_nodes().len()).map(|_| HashSet::new()).collect();
-    for (idx, node) in dag.get_nodes().iter().enumerate() {
-        for children in node.get_children() {
-            if let Some(child) = inverse_graph.get_mut(children.idx()) {
-                child.insert(idx);
-            } else {
-                panic!("{} is not a valid index", children.idx());
-            }
-        }
-    }
-    let dataless_graph: Vec<_> = dag
-        .get_nodes()
+pub fn kahn_algorithm<T: Debug>(dag: &Graph<T>) -> Result<Vec<NodeIndex>, Error> {
+    let mut parents_graph: BTreeMap<NodeIndex, _> = dag
+        .get_occupied_nodes()
         .iter()
-        .map(|n| n.get_children())
+        .map(|index| {
+            let node = dag.get_node_by_index(*index).expect("No node");
+            (*index, node.get_parents().clone())
+        })
         .collect();
 
-    let mut sorted_elements = Vec::new();
-    let mut nodes_without_incoming_edge = Vec::new();
-    for (idx, parents) in inverse_graph.iter().enumerate() {
-        if parents.is_empty() {
-            nodes_without_incoming_edge.push(idx);
+    let mut sorted_elements = Vec::with_capacity(dag.get_occupied_nodes().len());
+    let mut nodes_without_incoming_edge = VecDeque::with_capacity(dag.get_occupied_nodes().len());
+    for idx in dag.get_occupied_nodes() {
+        let node = dag.get_node_by_index(*idx).expect("Bad index");
+        if node.parents.is_empty() {
+            nodes_without_incoming_edge.push_back(*idx);
         }
     }
 
-    while let Some(node_idx) = nodes_without_incoming_edge.pop() {
+    while let Some(node_idx) = nodes_without_incoming_edge.pop_front() {
         sorted_elements.push(node_idx.into());
-        for child in dataless_graph[node_idx] {
-            inverse_graph[child.idx()].remove(&node_idx);
-            if inverse_graph[child.idx()].is_empty() {
-                nodes_without_incoming_edge.push(child.idx());
+        let node = dag.get_node_by_index(node_idx).expect("Bad index");
+        for child in node.get_children() {
+            let parent_node = parents_graph.get_mut(child).expect("No node");
+            parent_node.remove(&node_idx);
+            if parent_node.is_empty() {
+                nodes_without_incoming_edge.push_back(*child);
             }
         }
-        debug!("{:?}", sorted_elements);
-        debug!("{:?}", nodes_without_incoming_edge);
-        debug!("{:?}", inverse_graph);
+        debug!("b {:?}", sorted_elements);
+        debug!("c {:?}", nodes_without_incoming_edge);
+        debug!("d {:?}", parents_graph);
     }
 
-    for node in &inverse_graph {
+    for node in parents_graph.values() {
         if !node.is_empty() {
-            return Err(format_err!("Graph Has Cycles {:?}", inverse_graph));
+            return Err(format_err!("Graph Has Cycles {:?}", parents_graph));
         }
     }
 
     Ok(sorted_elements)
 }
 
-fn get_good_graph() -> GraphVariable<&'static str> {
-    let mut graph = GraphVariable::new();
-    let n0 = graph.push(None, IndexOrData::Data("n0")).unwrap();
-    let n1 = graph.push(n0, IndexOrData::Data("n1")).unwrap();
-    let n2 = graph.push(n0, IndexOrData::Data("n2")).unwrap();
-    let n3 = graph.push(n0, IndexOrData::Data("n3")).unwrap();
-    let n4 = graph.push(n1, IndexOrData::Data("n4")).unwrap();
-    graph.push(n2, IndexOrData::Index(n4)).unwrap();
-    graph.push(n3, IndexOrData::Data("n5")).unwrap();
-    let n6 = graph.push(n1, IndexOrData::Data("n6")).unwrap();
-    graph.push(n3, IndexOrData::Index(n6)).unwrap();
+fn get_good_graph() -> Graph<&'static str> {
+    let mut graph = Graph::new();
+    let n0 = graph.push_child(None, "n0").unwrap();
+    let n1 = graph.push_child(n0, "n1").unwrap();
+    let n2 = graph.push_child(n0, "n2").unwrap();
+    let n3 = graph.push_child(n0, "n3").unwrap();
+    let n4 = graph.push_child(n1, "n4").unwrap();
+    graph.push_index(n2, n4).unwrap();
+    graph.push_child(n3, "n5").unwrap();
+    let n6 = graph.push_child(n1, "n6").unwrap();
+    graph.push_index(n3, n6).unwrap();
+    let n7 = graph.push_child(n6, "n7").unwrap();
+    graph.push_index(n7, n4).unwrap();
     graph
 }
 
-fn get_bad_graph() -> GraphVariable<&'static str> {
-    let mut graph = GraphVariable::new();
-    let n0 = graph.push(None, IndexOrData::Data("n0")).unwrap();
-    let n1 = graph.push(n0, IndexOrData::Data("n1")).unwrap();
-    let n2 = graph.push(n0, IndexOrData::Data("n2")).unwrap();
-    let n3 = graph.push(n0, IndexOrData::Data("n3")).unwrap();
-    let n4 = graph.push(n1, IndexOrData::Data("n4")).unwrap();
-    graph.push(n2, IndexOrData::Index(n4)).unwrap();
-    graph.push(n3, IndexOrData::Data("n5")).unwrap();
-    let n6 = graph.push(n4, IndexOrData::Data("n6")).unwrap();
-    graph.push(n3, IndexOrData::Index(n6)).unwrap();
-    graph.push(n6, IndexOrData::Index(n1)).unwrap();
+fn get_bad_graph() -> Graph<&'static str> {
+    let mut graph = Graph::new();
+    let n0 = graph.push_child(None, "n0").unwrap();
+    let n1 = graph.push_child(n0, "n1").unwrap();
+    let n2 = graph.push_child(n0, "n2").unwrap();
+    let n3 = graph.push_child(n0, "n3").unwrap();
+    let n4 = graph.push_child(n1, "n4").unwrap();
+    graph.push_index(n2, n4).unwrap();
+    graph.push_child(n3, "n5").unwrap();
+    let n6 = graph.push_child(n4, "n6").unwrap();
+    graph.push_index(n3, n6).unwrap();
+    graph.push_index(n6, n1).unwrap();
     graph
 }
 
